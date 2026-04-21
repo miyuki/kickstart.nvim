04419180d63ab79f90aa21fac43269f72739a92c
Mikhail Maltsev <mikhail.maltsev@arm.com>
2026-04-21 15:33:58 +0100

    Update Neovim local config behavior

--- init.lua
+++ init.lua
@@ -104,8 +104,8 @@ vim.o.number = true
 --  Experiment for yourself to see if you like it!
 -- vim.o.relativenumber = true
 
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = 'a'
+-- Keep mouse handling in the terminal emulator instead of Neovim.
+vim.o.mouse = ''
 
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
@@ -114,7 +114,11 @@ vim.o.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+if vim.env.SSH_CONNECTION or vim.env.SSH_CLIENT or vim.env.SSH_TTY then
+  vim.opt.clipboard = ''
+else
+  vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
+end
 
 -- Enable break indent
 vim.o.breakindent = true
@@ -927,7 +931,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,33 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'romgrk/barbar.nvim',
+    version = '^1.0.0',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+      vim.opt.showtabline = 2
+    end,
+    opts = {
+      animation = false,
+    },
+    keys = {
+      { '<F1>', '<Cmd>BufferGoto 1<CR>', mode = 'n', desc = 'Buffer 1' },
+      { '<F2>', '<Cmd>BufferGoto 2<CR>', mode = 'n', desc = 'Buffer 2' },
+      { '<F3>', '<Cmd>BufferGoto 3<CR>', mode = 'n', desc = 'Buffer 3' },
+      { '<F4>', '<Cmd>BufferGoto 4<CR>', mode = 'n', desc = 'Buffer 4' },
+      { '<F5>', '<Cmd>BufferGoto 5<CR>', mode = 'n', desc = 'Buffer 5' },
+      { '<F6>', '<Cmd>BufferGoto 6<CR>', mode = 'n', desc = 'Buffer 6' },
+      { '<F7>', '<Cmd>BufferGoto 7<CR>', mode = 'n', desc = 'Buffer 7' },
+      { '<F8>', '<Cmd>BufferGoto 8<CR>', mode = 'n', desc = 'Buffer 8' },
+      { '<F9>', '<Cmd>BufferGoto 9<CR>', mode = 'n', desc = 'Buffer 9' },
+      { '<F10>', '<Cmd>BufferLast<CR>', mode = 'n', desc = 'Last buffer' },
+      { '<F11>', '<Cmd>BufferPrevious<CR>', mode = 'n', desc = 'Previous buffer' },
+      { '<F12>', '<Cmd>BufferNext<CR>', mode = 'n', desc = 'Next buffer' },
+    },
+  },
+}
